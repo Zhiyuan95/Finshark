cedb725553afb5cf0c102fd3e2e7002710c67c21
Zhiyuan95 <vasiazhaaang@gmail.com>
2025-05-22 09:01:19 +1000

    add components and set up axios

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,6 +1,10 @@
-import React from "react";
-
-const Card = () => {
+import React, { JSX } from "react";
+import { CompanySearch } from "../../company";
+interface Props {
+  searchResult: CompanySearch;
+  id:string;
+}
+const Card: React.FC<Props> = ({ searchResult, id }: Props): JSX.Element => {
   return <div>Card</div>;
 };
 
--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -1,15 +1,23 @@
 import React, { JSX } from "react";
 import Card from "../Card/Card";
+import { CompanySearch } from "../../company";
+import { v4 as uuidv4 } from "uuid";
 
-interface Props {}
+interface Props {
+  searchResults: CompanySearch[];
+}
 
-const CardList: React.FC<Props> = (props: Props):JSX.Element => {
+const CardList: React.FC<Props> = ({ searchResults }: Props): JSX.Element => {
   return (
-    <div>
-      <Card />
-      <Card />
-      <Card />
-    </div>
+    <>
+      {searchResults.length > 0 ? (
+        searchResults.map((result) => {
+          return <Card id={result.symbol} key={uuidv4()} searchResult={result} />;
+        })
+      ) : (
+        <h1>No Result</h1>
+      )}
+    </>
   );
 };
 
